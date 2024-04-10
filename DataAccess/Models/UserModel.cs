3e6cd53059c076627186ab6b8f3d679e2b675acb
funwithlasers <mjlarson13@gmail.com>
2024-04-09 19:19:29 -0500

    pw to string

--- DataAccess/Models/UserModel.cs
+++ DataAccess/Models/UserModel.cs
@@ -6,6 +6,6 @@ public class UserModel
     public string Email { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public byte[] Password { get; set; }
+    public string Password { get; set; }
 
 }
