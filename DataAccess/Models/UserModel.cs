162fb4d9400fe4a0a0fb65ab282b27525d5df890
funwithlasers <mjlarson13@gmail.com>
2024-04-09 19:10:04 -0500

    add email

--- DataAccess/Models/UserModel.cs
+++ DataAccess/Models/UserModel.cs
@@ -3,6 +3,9 @@
 public class UserModel
 {
     public int UserId { get; set; }
+    public string Email { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public byte[] Password { get; set; }
+
 }
